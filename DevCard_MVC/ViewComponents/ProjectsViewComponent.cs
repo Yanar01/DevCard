22af77ab04984ac6310f08663ff661fd0c84844d
Yanar Asaei <yanar.asaei@gmail.com>
2021-03-21 11:00:11 +0330

    make Projects ViewComponent

---
+++ DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Project>
+            {
+                new Project(1, "name1", "description1", "client1","project-1.jpg"),
+                new Project(2, "name2", "description2", "client2","project-2.jpg"),
+                new Project(3, "name3", "description3", "client3","project-3.jpg"),
+                new Project(4, "name4", "description4", "client4","project-4.jpg"),
+            };
+            return View("_Projects",projects);
+        }
+    }
+}
