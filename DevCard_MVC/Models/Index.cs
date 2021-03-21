5b20cded7ab32c31cfaa3c369687549c27a9c30e
Yanar Asaei <yanar.asaei@gmail.com>
2021-03-21 10:23:20 +0330

    move projects section from index to partial view Projects

---
+++ DevCard_MVC/Models/Index.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+
+namespace DevCard_MVC.Models
+{
+    public class Index
+    {
+        public List<Project> Projects { get; set; }
+        public List<Article> Articles { get; set; }
+    }
+}
