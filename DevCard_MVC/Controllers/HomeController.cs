be7103df5f786c52b0fa2880e9ab1d7df101ba7a
Yanar Asaei <yanar.asaei@gmail.com>
2021-03-23 14:32:08 +0430

    add SelectList and use in Contact page

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using DevCard_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DevCard_MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly List<Service> _services = new()
+        {
+            new Service(1, "سطح ۱"),
+            new Service(2, "سطح ۲"),
+            new Service(3, "سطح ۳"),
+            new Service(4, "سطح ۴")
+        };
+
         public HomeController(ILogger<HomeController> logger)
         {
         }
@@ -21,7 +31,10 @@ namespace DevCard_MVC.Controllers
         [HttpGet]
         public IActionResult Contact()
         {
-            var model = new Contact();
+            var model = new Contact()
+            {
+                Services = new SelectList(_services, "Id", "Name")
+            };
             return View(model);
         }
 
@@ -33,10 +46,22 @@ namespace DevCard_MVC.Controllers
         //}
 
         [HttpPost]
-        public JsonResult Contact(Contact form)
+        public IActionResult Contact(Contact model)
         {
-            Console.WriteLine(form.ToString());
-            return Json(Ok());
+            model.Services = new SelectList(_services, "Id", "Name");
+            if (!ModelState.IsValid)
+            {
+                ViewData["error"] = "اطلاعات وارد شده صحیح نمی‌باشد";
+                return View(model);
+            }
+
+            ModelState.Clear();
+            model = new Contact()
+            {
+                Services = new SelectList(_services, "Id", "Name")
+            };
+            ViewData["success"] = "پیغام شما با موفقیت ثبت شد";
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- DevCard_MVC/Models/Contact.cs
+++ DevCard_MVC/Models/Contact.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DevCard_MVC.Models
 {
@@ -18,6 +19,8 @@ namespace DevCard_MVC.Models
         public string Email { get; set; }
 
         public string Message { get; set; }
-        public string Service { get; set; }
+        public int Service { get; set; }
+
+        public SelectList Services { get; set; }
     }
 }
